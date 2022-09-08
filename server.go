41e41ce09879d3f691a14e8cd69184bfb25ff16d
linho1150 <56428918+Linho1150@users.noreply.github.com>
2022-09-08 14:21:53 +0900

    Fix: 템플릿 엔진 적용 예시 및 Static 폴더 접근 권한 설정

--- server.go
+++ server.go
@@ -1,12 +1,42 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func setupRouter() *gin.Engine {
 	server := gin.Default()
+	server.Static("/static", "./static")
 	server.LoadHTMLGlob("templates/*")
-	server.GET("/", func(reposne *gin.Context) {
-		reposne.String(200, "Hello World")
+	server.GET("/", func(response *gin.Context) {
+		response.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Home Page",
+		},
+		)
+	})
+	server.GET("/urls", func(response *gin.Context) {
+		response.HTML(http.StatusOK, "result.html", gin.H{
+			"innerUrl": "www.naver.com",
+			"outerUrl": "www.google.com",
+		},
+		)
+	})
+	server.GET("/statements", func(response *gin.Context) {
+		response.HTML(http.StatusOK, "statement.html", gin.H{
+			"accessMin":    "Home Page",
+			"accessDay":    "Home Page",
+			"accessDevice": "Home Page",
+			"referer":      "Home Page",
+		},
+		)
+	})
+	server.PUT("/urls", func(response *gin.Context) {
+		response.String(200, "Hi")
+	})
+	server.DELETE("/urls", func(response *gin.Context) {
+		response.String(200, "Hi")
 	})
 	return server
 }
