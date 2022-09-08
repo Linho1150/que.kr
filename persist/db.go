aeecf9a967502d03fc33eb3715b79c9c503d68e4
wwoopp <wwoopp@wwoopp.com>
2022-09-08 17:40:23 +0000

    USELOCALDYNAMODB 환경 변수에 따라 로컬 접속 여부를 결정짓도록 함

--- persist/db.go
+++ persist/db.go
@@ -3,22 +3,42 @@ package persist
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
+func checkUseLocalDynamoDbFlag() bool {
+	useLocalDynamoDbFlagStr := os.Getenv("USELOCALDYNAMODB")
+
+	if useLocalDynamoDbFlagStr != "" {
+		return true
+	}
+
+	return false
+}
+
 func CreateDynamoDbClient() (*dynamodb.Client, error) {
+	var cfgOptFns []func(*config.LoadOptions) error
+
+	if checkUseLocalDynamoDbFlag() {
+		cfgOptFns = append(
+			cfgOptFns,
+			config.WithEndpointResolverWithOptions(
+				aws.EndpointResolverWithOptionsFunc(
+					func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+						return aws.Endpoint{URL: "http://localhost:8000"}, nil
+					},
+				),
+			),
+		)
+	}
+
 	cfg, err := config.LoadDefaultConfig(
 		context.TODO(),
-		config.WithEndpointResolverWithOptions(
-			aws.EndpointResolverWithOptionsFunc(
-				func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-					return aws.Endpoint{URL: "http://localhost:8000"}, nil
-				},
-			),
-		),
+		cfgOptFns...,
 	)
 
 	if err != nil {
