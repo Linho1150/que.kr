a4d4725c750d12351b758a830843332fa6921d77
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2022-09-10 18:10:24 +0900

    Feat: Client Server 간의 통신 규약 설정

--- server.go
+++ server.go
@@ -25,17 +25,17 @@ func setupRouter() *gin.Engine {
 	})
 	server.GET("/statements", func(response *gin.Context) {
 		response.HTML(http.StatusOK, "statement.html", gin.H{
-			"accessMin":    "Home Page",
-			"accessDay":    "Home Page",
-			"accessDevice": "Home Page",
-			"referer":      "Home Page",
+			"accessMin": `5분전/4분전/3분전/2분전/1분전,23/24/25/26/27`,
+			"accessDay": `23일/24일/25일/26일/27일,23/24/25/26/27`,
+			"accessDevice": "20/50/30", //Moblie, Web, Etc
+			"referer": `naver.com/32,daum.net/21,google.com/12,tistory.com/3,kakao.com/6,linho.kr/1`,
 		},
 		)
 	})
 	server.PUT("/urls", func(response *gin.Context) {
 		response.String(200, "Hi")
 	})
-	server.DELETE("/urls", func(response *gin.Context) {
+	server.DELETE("/urls", func(response *gin.Context) {	
 		response.String(200, "Hi")
 	})
 	return server
