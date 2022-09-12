0a289d0aae1fba79b230c29b543fdb353acb1b2f
wwoopp <wwoopp@wwoopp.com>
2022-09-12 13:41:47 +0000

    통계 자료 적재 부분을 practice_service.go로 옮김

--- run_statistics_handler.go
+++ run_statistics_handler.go
@@ -2,24 +2,6 @@
 
 package main
 
-import (
-	"fmt"
-	"quekr/server/service"
-	"time"
-)
-
 func main() {
-	svc, err := service.NewService()
 
-	if err != nil {
-		panic(err)
-	}
-
-	err = svc.TouchStatistics("abc", time.Now(), "127.0.0.1", "https://daum.net", service.DeviceTypePC)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("go!")
 }
