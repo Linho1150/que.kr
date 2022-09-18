69c479d8a745d86afcdd3f47b7325f77d06427bd
LeeJaeJun <56428918+Linho1150@users.noreply.github.com>
2022-09-18 23:07:46 +0900

    style: Method 설명 추가

--- practice_service.go
+++ practice_service.go
@@ -10,40 +10,40 @@ import (
 
 func main() {
 	svc, err := service.NewService()
-
+	// define service
 	if err != nil {
 		panic(err)
 	}
 
 	info, err := svc.CreateMapping("https://naver.com", "127.0.0.1")
-
+	// make s	hortUrl
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("created")
 	fmt.Println(info)
-
+	
 	err = svc.UpdateMapping(info.ShortKey, "abc", "https://daum.net")
-
+	// this is bad example
 	if err == nil {
 		panic("update why succeed?")
 	}
 
 	err = svc.UpdateMapping("notexist", "abc", "https://daum.net")
-
+	// this is bad example
 	if err == nil {
 		panic("update why succeed?")
 	}
-
+	
 	err = svc.UpdateMapping(info.ShortKey, info.SecretToken, "https://daum.net")
-
+	
 	if err != nil {
 		panic(err)
 	}
 
 	info, err = svc.QueryMapping(info.ShortKey)
-
+	// get original url
 	if err != nil {
 		panic(err)
 	}
@@ -52,7 +52,6 @@ func main() {
 
 	// queuing raw access record for accumlating statistics info
 	err = svc.TouchStatistics(info.ShortKey, svc.NowLocalTime(), "127.0.0.1", "https://referer", service.DeviceTypePC)
-
 	if err != nil {
 		panic(err)
 	}
@@ -60,7 +59,7 @@ func main() {
 	time.Sleep(time.Second * 40)
 
 	starows, err := svc.QueryStatistics(info.ShortKey, service.StatisticLegendTypeReferer, false)
-
+	// getStatistics information (false = asc or desc)
 	if err != nil {
 		panic(err)
 	}
@@ -70,7 +69,7 @@ func main() {
 	}
 
 	err = svc.RemoveMapping(info.ShortKey, info.SecretToken)
-
+	// remove mapping
 	if err != nil {
 		panic(err)
 	}
