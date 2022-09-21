881166e31920c09294d5dd63af4a6b114febb838
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2022-09-22 00:50:05 +0900

    Fix: Statistics 처리 중 발생한 문제 해결 중 #9

--- server.go
+++ server.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"quekr/server/service"
 
@@ -51,7 +53,77 @@ func setupRouter() *gin.Engine {
 		},
 		)
 	})
-	server.GET("/statements", func(response *gin.Context) {
+	server.GET("/:shortkey", func(response *gin.Context) {
+		ipAddress := ReadUserIP(response.Request)
+		referer := ReadUserReferer(response.Request)
+		shortkey := response.Param("shortkey")
+
+		deviceType := service.DeviceTypePC
+		mobileDevice := isMobile(response.Request)
+		if(mobileDevice){
+			deviceType = service.DeviceTypeMobile
+		}
+
+		item, err := svc.QueryMapping(shortkey)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(shortkey,ipAddress,referer,deviceType)
+		err = svc.TouchStatistics(shortkey, svc.NowLocalTime(), ipAddress, referer, deviceType)
+		if err != nil {
+			panic(err)
+		}
+    response.Redirect(http.StatusTemporaryRedirect, "http://"+item.OriginalUrl)
+	})
+
+	server.GET("/:shortkey/:secrettoken", func(response *gin.Context) {
+		shortkey := response.Param("shortkey")
+		secrettoken := response.Param("secrettoken")
+
+		info, err := svc.QueryMapping(shortkey)
+		if err != nil {
+			panic(err)
+		}
+
+		if info.SecretToken != secrettoken{
+			panic("Authentication failure");
+		}
+
+		dataReferer, err := svc.QueryStatistics(shortkey, service.StatisticLegendTypeReferer, false)
+		if err != nil {
+			panic(err)
+		}
+		dataDeviceType, err := svc.QueryStatistics(shortkey, service.StatisticLegendTypeDevicetype, false)
+		if err != nil {
+			panic(err)
+		}
+
+		dataTimerPerDate, err := svc.QueryStatistics(shortkey, service.StatisticLegendTypeTimePerDate, false)
+		if err != nil {
+			panic(err)
+		}
+
+		dataTimePerMinute, err := svc.QueryStatistics(shortkey, service.StatisticLegendTypeTimePerMinute, false)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, starow := range dataReferer {
+			fmt.Printf("%s => %d \n", starow.Legend.(string), starow.Counter)
+		}
+
+		for _, starow := range dataDeviceType {
+			fmt.Printf("%s => %d \n", starow.Legend.(string), starow.Counter)
+		}
+
+		for _, starow := range dataTimePerMinute {
+			fmt.Printf("Minute: %s => %d \n", starow.Legend, starow.Counter)
+		}
+
+		for _, starow := range dataTimerPerDate {
+			fmt.Printf("Date: %s => %d \n", starow.Legend, starow.Counter)
+		}
+		
 		response.HTML(http.StatusOK, "statement.html", gin.H{
 			"accessMin": `5분전/4분전/3분전/2분전/1분전,23/24/25/26/27`,
 			"accessDay": `23일/24일/25일/26일/27일,23/24/25/26/27`,
@@ -59,21 +131,27 @@ func setupRouter() *gin.Engine {
 			"referer": `naver.com/32,daum.net/21,google.com/12,tistory.com/3,kakao.com/6,linho.kr/1`,
 		},
 		)
+
 	})
-	server.GET("/:shortkey", func(response *gin.Context) {
+	server.PUT("/:shortkey/:secrettoken", func(response *gin.Context) {
 		shortkey := response.Param("shortkey")
-		item, err := svc.QueryMapping(shortkey)
+		secrettoken := response.Param("secrettoken")
+		err = svc.UpdateMapping(shortkey, secrettoken, "https://daum.net")
+		//todo: 변경할 URL 어떻게 가져올지 고민하기
+	
 		if err != nil {
 			panic(err)
 		}
-    response.Redirect(http.StatusTemporaryRedirect, "http://"+item.OriginalUrl)
+		response.String(200, "update")
 	})
-
-	server.PUT("/urls", func(response *gin.Context) {
-		response.String(200, "Hi")
-	})
-	server.DELETE("/urls", func(response *gin.Context) {	
-		response.String(200, "Hi")
+	server.DELETE("/:shortkey/:secrettoken", func(response *gin.Context) {	
+		shortkey := response.Param("shortkey")
+		secrettoken := response.Param("secrettoken")
+		err = svc.RemoveMapping(shortkey, secrettoken)
+		if err != nil {
+			panic(err)
+		}
+		response.String(200, "remove")
 	})
 	return server
 }
@@ -92,4 +170,16 @@ func ReadUserIP(r *http.Request) string {
 			IPAddress = r.RemoteAddr
 	}
 	return IPAddress
+}
+
+func ReadUserReferer(r *http.Request) string {
+	return r.Header.Get("referer")
+}
+
+func isMobile(r *http.Request) bool  {
+	userAgent:=r.Header.Get("User-Agent")
+	if(strings.Contains(userAgent,"Mobi")){
+		return true
+	}
+	return false
 }
